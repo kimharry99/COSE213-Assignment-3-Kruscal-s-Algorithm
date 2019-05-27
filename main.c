b019e6b6b66239d9bcb38700de597ebd1d8d4a10
kimharry9927 <kimharry9927@gmail.com>
2019-05-27 17:42:42 +0900

    implement input error handle function goodScanf

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_ELEMENTS 50
+
+
+
 
 /*
 * implement the graph as an adjacency list with weight field in a each node
 * input : pointer of adjList
 * output : pointer of resultAdjList
 * V(outputGraph) == V(inputGraph)
+* E(outputGraph) ∈ E(inputGraph)
 */
+
+
+/*
+구현을 하고 전역변수를 줄이기
+구현 순서 
+* TODO : 
+inputGraph
+printGraph
+initializeOutputGraph
+KrukalsAlgorithm
+edgesort
+*DONE :
+main
+*/
+
 
 /* struct */
 
@@ -26,7 +47,7 @@ typedef struct _node {
 
 /* global variables */
 
-nodePointer arrinputAdjList[MAX_ELEMENTS], arrOutputAdjList[MAX_ELEMENTS];
+nodePointer arrInputAdjList[MAX_ELEMENTS], arrOutputAdjList[MAX_ELEMENTS];
 /* use for examining that any cycle is created in the graph. */
 int arrResultVertexParent[MAX_ELEMENTS] = { -1, }; 
 /* use for ascending sorting of edge weights */
@@ -40,17 +61,32 @@ void unionVertexTree(int, int);
 	if(findElementRoot(i)==findElementRoot(j)) don't add edge(i,j) to tree T	*/
 int findElementRoot(int);
 /*	inialize output graph.
-	set links of elements of arrOutputAdjList[] null.	*/
-void createInitialOutputGraph();
+	set numbers of elements in arrOutputAdjList equals to arrInputAdjList.
+	set links of elements of arrOutputAdjList[] null.	
+	save edge data to arrEdge.
+	modify arrResultVertexParent
+*/
+void initializeOutputGraph();
 /* sort arrEdge in ascending order by the weight of the edges */
-void edgeSort();
+void sortEdge();
 /*	add edge to output graph.
 	return 1 : adding success, return 0 : adding failure */
 int addEdge(edge);
 void KrusklsAlgorithm();
 void inputGraph();
 void printGraph();
+/* handle not integer exceptions and out of range exception. */
+int goodScanf(int,int);
 
+int main() {
+	/*inputGraph();
+	initializeOutputGraph();
+	sortEdge();
+	KrusklsAlgorithm();
+	printGraph();
+	*/
+	return 0;
+}
 
 void unionVertexTree(int i, int j) 
 {	
@@ -75,4 +111,93 @@ int findElementRoot(int i)
 		arrResultVertexParent[trail] = root;
 	}
 	return root;
+}
+
+void initializeOutputGraph() {
+
+}
+
+void sortEdge() {
+
+}
+
+int addEdge(edge _edge) {
+
+}
+
+void KrusklsAlgorithm() {
+	/*	
+	while(n(edge)<n(vertex)){
+	edge배열에서 하나 꺼내옴
+	cycle을 이루는지 검사
+	cycle을 이루지 않으면 T에 edge추가
+	}
+	*/
+}
+
+int goodScanf(int rangeA, int rangeB) {
+	/* good range = [rangeA, rangeB] */
+	int n;
+	if (scanf("%d", &n));
+	else {
+		printf("wrong input.(not an integer)");
+		exit(EXIT_FAILURE);
+	}
+	/* exit program since n is smaller than rangeA */
+	if (n<rangeA) {
+		printf("Wrong input. (out of range)");
+		exit(EXIT_FAILURE);
+	}
+	else {
+		/* exit program since n is bigger than _range */
+		if (n > rangeB) {
+			printf("Wrong input. (out of range)");
+			exit(EXIT_FAILURE);
+		}
+		else return n;
+	}
+	printf("Error in the function int goodScanf()");
+	exit(EXIT_FAILURE);
+}
+
+void inputGraph() {
+	int n = 0;
+	/*
+	* input vertex : number of vertex >> 0~n
+	* exceptions :
+	*	not integer
+	*	out of range (good range = [1,MAX_ELEMENTS])
+	*/
+	printf("input number of vertex>>");
+	if (!scanf("%d", &n)) {
+
+	}
+
+
+	/*
+	* input edges : head and tail and weight 
+	* need an end signal (maybe enter -1 at first data)
+	* exceptions
+	*	input wrong tail 
+	*		not integer
+	*		out of range(good range = [0,number of vertexes))
+	*	input wrong head 
+	*		not integer
+	*		out of range(good range = [0,number of vertexes))
+	*		same with tail
+	*		already exsited
+	*	input wrong weight
+	*		not integer
+	*		out of range (good range = [0,∞)) 
+	*/
+
+	/* make adjecentList */
+
+	/*	exceptions 
+	*		graph is not connected 
+	*/
+}
+
+void printGraph() {
+
 }
